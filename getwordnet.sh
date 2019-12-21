5bea884a645d33a5514faae8042ee0d005e1c8c3
Xinyu Zhao <xinyuzhao@utexas.edu>
2019-12-21 15:40:17 -0600

    fixed getwordnet.sh to work on ubuntu

--- getwordnet.sh
+++ getwordnet.sh
@@ -4,18 +4,20 @@
 # 2. Creates a jwnl_file_properties.xml file to point to the downloaded data files.
 #
 
-mkdir ~/wordnet3.1
+BASE_DIR=.
+
+mkdir $BASE_DIR/wordnet3.1
 
 # Create the WordNet properties file.
-cp src/test/resources/jwnl_file_properties.xml ~/wordnet3.1/
-pushd ~/wordnet3.1
+cp src/test/resources/jwnl_file_properties.xml $BASE_DIR/wordnet3.1/
+pushd $BASE_DIR/wordnet3.1
 
 # Get the WordNet dictionary files.
 # Use curl to download, as OS X doesn't include wget
-curl -O http://wordnetcode.princeton.edu/wn3.1.dict.tar.gz
-gunzip wn3.1.dict.tar.gz
-tar -xvf wn3.1.dict.tar
-rm wn3.1.dict.tar
+wget http://wordnetcode.princeton.edu/wn3.1.dict.tar.gz
+tar -xf wn3.1.dict.tar.gz
+
+rm wn3.1.dict.tar.gz
 
 # Put the current path into the properties file.
 MYPWD=`pwd`
@@ -27,3 +29,4 @@ popd
 echo "A new directory was created at $MYPWD"
 echo "YOU MUST create an environment variable JWNL that points to $MYPWD/jwnl_file_properties.xml"
 echo "Recommdation: add it to your .bashrc"
+
